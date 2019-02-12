c85a16bb1a7563ac015f96581ebd1f29befdaf77
Aayush Gupta <aayushgupta2097@gmail.com>
2019-02-12 14:10:22 +0530

    circle and SI

---
+++ Javascript/Circle/circle.js
@@ -0,0 +1,3 @@
+var r = prompt("Enter the radius of circle: ") 
+var pi = 3.14
+alert("The area of the circle is " + (pi*r*r)) ; 
---
+++ Javascript/Simple interest/demo.js
@@ -0,0 +1,10 @@
+var p = prompt("Enter the principal amount: ")
+var r = prompt("Enter the rate of interest: ")
+r = r/100 ; 
+var t = prompt("Enter the time: ")
+
+S = p*r*t ; 
+alert("The Value of Simple Interest is : " + S) ;
+
+alert("The total amout is: " + (p*(1+r*t))) ;
+
