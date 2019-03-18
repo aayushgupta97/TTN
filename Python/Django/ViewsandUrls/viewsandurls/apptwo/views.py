913e77d84859c916c6731b3836b342fa4c3cf2e8
Aayush Gupta <aayushgupta2097@gmail.com>
2019-03-18 14:18:19 +0530

    adding views and urls exercise

---
+++ Python/Django/ViewsandUrls/viewsandurls/apptwo/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def wish_user(request,input_num):
+	if input_num >=0 and input_num <= 24:
+	    if 5 < input_num and input_num < 12:
+	        wish='Good Morning'
+	    elif 12 <= input_num and input_num < 18:
+	        wish='Good Evening'
+	    elif 18 <= input_num and input_num < 24:
+	        wish='Good Night'
+	    else:
+	        wish='Go To Bed'
+	    return HttpResponse(wish)
+	else: 
+		return HttpResponse("Invalid Time try again!")
---
+++ Python/Django/ViewsandUrls/viewsandurls/viewsandurls/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from appone import views as app_one_view
+from apptwo import views as app_two_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('<int:input_num>',app_two_view.wish_user,name='function_two'),
+    path('<filename>',app_one_view.function_one,name='function_one')
+]
