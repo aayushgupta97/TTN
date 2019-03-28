d1d7a025f0a2025b420f3b68a779dc088828aeb7
Aayush Gupta <aayushgupta2097@gmail.com>
2019-03-28 14:20:05 +0530

    adding urls to template, userauth and sessions

--- Python/Django/UserAuthentication/myproject/urls.py
+++ Python/Django/UserAuthentication/myproject/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from userauth import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index , name = "index"), 
-    path('logout/', views.user_logout, name = "logout"),
+    path('userauth/', include('userauth.urls')),
+
 ]
